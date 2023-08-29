7920dc683e33957ab694f4f21f180713dab0eadc
meow0222 <meow222.csgo@gmail.com>
2023-08-30 08:46:36 +0900

    Add popup component /mao

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,37 @@
 import './Products.css'
+import Popup from "./Popup";
+import React, { useState } from 'react'
 
 export function Products ({ products }) {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedProductImages, setSelectedProductImages] = useState('');
+    const [selectedProductTitles, setSelectedProductTitles] = useState('');
+    const [selectedProductPrices, setSelectedProductPrices] = useState('');
+    const [selectedProductDescription, setSelectedProductDescription] = useState('');
 
     const onAddProduct = () => {
         console.log("add")
     }
+
+    const ShowModal = (product) => {
+        setSelectedProductImages(product.images);
+        setSelectedProductTitles(product.title);
+        setSelectedProductPrices(product.price);
+        setSelectedProductDescription(product.description);
+
+        setShowModal(true);
+      };
 
     return (
         <main className='products'>
             <ul>
                 {products.map(product => (
                     <li key={product.id}>
-                        <img src={product.thumbnail} alt={product.title} />
+                        <img 
+                        src={product.thumbnail} 
+                        alt={product.title}
+                        onClick={() => ShowModal(product)}
+                        />
                         <div>
                             <strong>{product.title} ${product.price}</strong>
                         </div>
@@ -23,6 +43,14 @@ export function Products ({ products }) {
                     </li>
                 ))}
             </ul>
+            <Popup 
+            showFlag={showModal} 
+            setShowModal={setShowModal}
+            productImages={selectedProductImages}
+            productTitles={selectedProductTitles}
+            productPrices={selectedProductPrices}
+            productDescription={selectedProductDescription}
+            />
         </main>
     )
 }
