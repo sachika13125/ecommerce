b763faceb2738708c5d17c616ddfcd529295625f
meow0222 <meow222.csgo@gmail.com>
2023-08-31 09:19:11 +0900

    Fix Popup.jsx /mao

--- src/components/SearchBarContainer.jsx
+++ src/components/SearchBarContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Popup from "./Popup";
+import Popup from "./Popup.jsx";
 import { FaSearch } from 'react-icons/fa';
 import './SearchBarContainer.css';
 
